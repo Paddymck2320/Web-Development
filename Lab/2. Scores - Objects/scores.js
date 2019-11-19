44d4345d103c783e35e20846055ebb692529d1dc
Paddymck2320 <56718903+Paddymck2320@users.noreply.github.com>
2019-11-19 14:51:58 +0000

    jf

--- Lab/2. Scores - Objects/scores.js
+++ Lab/2. Scores - Objects/scores.js
@@ -1,17 +1,55 @@
 var $ = function (id) { return document.getElementById(id); };
 
-var displayScores = function () {   
+var Students =  [];
+var personNumber = 2;
+
+var Student = function(firstname, lastname, grade){
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.grade = grade;
+}
+
+var displayScores = function () { 
+    $("scores").value = " ";
+
+    if(Students.length === 0){
+        Students = localStorage.getItem("Students")|| "";
+        Students = JSON.parse(localStorage.Students);
+    }
+    for(var i = 0; i < Students.length; i++)
+    {
+        var score = "";
+        score += "," + Students[i].lastname;
+        score += ":" + Students[i].firstname;
+        score += "\n";
+        $("scores").value += score;
+    } 
     
+    var average = 0;
+    for(var j = 0; j < Students.length; j++)
+    {
+        var t =  parseInt(Students[j].grade);
+        average += t;
+    }
+    average = average / Students.length;
+    $("average_score").value = average.toFixed(2);
 };
 
 var addScore = function () {
-    
+    var firstname = $("first_name").value;
+    var lastname = $("last_name").value;
+    var grade = $("score").value;
+
+    var person = new Student(firstname, lastname, grade);
+
+    Students.push(person);
     
     // get the add form ready for next entry
     $("first_name").value = "";
     $("last_name").value = "";
     $("score").value = "";
     $("first_name").focus();
+    displayScores();
 };
 
 var clearScores = function () {   
@@ -24,8 +62,9 @@ var clearScores = function () {
     $("first_name").focus();
 };
 
-var sortScores = function () {   
-     
+var sortScores = function () {  
+    Students = Students.sort(); 
+     console.log(Students);
 };
 
 window.onload = function () {
@@ -33,4 +72,5 @@ window.onload = function () {
     $("clear_button").onclick = clearScores;    
     $("sort_button").onclick = sortScores;    
     $("first_name").focus();
+    displayScores();
 };
