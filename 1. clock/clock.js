2453cdfd2b54c6aefe1fd323c9531dbcc874aacf
Paddymck2320 <56718903+Paddymck2320@users.noreply.github.com>
2019-11-26 13:20:41 +0000

    lab10

---
+++ 1. clock/clock.js
@@ -0,0 +1,14 @@
+"use strict";
+var $ = function(id) { return document.getElementById(id); };
+
+var displayCurrentTime = function() {
+
+};
+
+var padSingleDigit = function(num) {
+    return (num < 10) ? "0" + num : num;
+};
+
+window.onload = function() {
+    
+};
