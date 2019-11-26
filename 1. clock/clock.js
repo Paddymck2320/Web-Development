a8da9dcd374d8774cc5a52120cd4261a90f6d91a
Paddymck2320 <56718903+Paddymck2320@users.noreply.github.com>
2019-11-26 13:51:03 +0000

    lab 10 finished

--- 1. clock/clock.js
+++ 1. clock/clock.js
@@ -2,7 +2,17 @@
 var $ = function(id) { return document.getElementById(id); };
 
 var displayCurrentTime = function() {
-
+    var day = new Date();
+    let hours = day.getHours();
+    let minutes = day.getMinutes();
+    let seconds = day.getSeconds();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    
+    $("hours").firstChild.nodeValue = hours;
+    $("minutes").firstChild.nodeValue = minutes;
+    $("seconds").firstChild.nodeValue = seconds;
+    $("ampm").firstChild.nodeValue = ampm;
 };
 
 var padSingleDigit = function(num) {
@@ -10,5 +20,7 @@ var padSingleDigit = function(num) {
 };
 
 window.onload = function() {
+    setInterval(displayCurrentTime, 1000);
+    displayCurrentTime();
     
 };
